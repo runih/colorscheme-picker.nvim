25afea053ef4c1c2021eb353c039e1b8c3336a0b
Rúni H.Hansen <runi.hansen@okkara.net>
2023-12-01 15:15:05 +0100

    Fixed a nil issue

--- lua/colorscheme-picker/init.lua
+++ lua/colorscheme-picker/init.lua
@@ -6,68 +6,76 @@ local colorscheme_file = vim.fn.expand("~/.config/nvim/colorscheme")
 local colorscheme = {}
 
 colorscheme.change = function(selected_colorscheme)
-  vim.cmd("colorscheme " .. selected_colorscheme)
-  if vim.g.neovide then
-    local normal_highlight = vim.api.nvim_get_hl_by_name("Normal", true)
-    if normal_highlight and normal_highlight.background then
-      vim.g.neovide_background_color = string.format("%06x", normal_highlight.background)
-      .. string.format("%x", (255 * vim.g.transparency))
-    end
-  end
+	vim.cmd("colorscheme " .. selected_colorscheme)
+	if vim.g.neovide then
+		local normal_highlight = vim.api.nvim_get_hl_by_name("Normal", true)
+		if
+			normal_highlight
+			and normal_highlight.background
+			and vim.g.neovide_background_color
+			and vim.g.transparency
+		then
+			vim.g.neovide_background_color = string.format("%06x", normal_highlight.background)
+				.. string.format("%x", (255 * vim.g.transparency))
+		end
+	end
 end
-
 
 colorscheme.set_default_colorscheme = function()
-  local file = io.open(colorscheme_file, "r")
-  if file then
-    current_colorscheme = file:read()
-    file:close()
-  end
-  colorscheme.change(current_colorscheme)
+	local file = io.open(colorscheme_file, "r")
+	if file then
+		current_colorscheme = file:read()
+		file:close()
+	end
+	colorscheme.change(current_colorscheme)
 end
 
-
 colorscheme.setup = function(opts)
-  if opts.default_colorscheme then
-    current_colorscheme = opts.default_colorscheme
-  end
-  if opts.nvim_config then
-    colorscheme_file = opts.nvim_config .. "/colorscheme"
-  end
-  if opts.keymapping then
-    keymapping = opts.keymapping
-  end
-  local builtin_loaded, builtin = pcall(require, "telescope.builtin")
-  if builtin_loaded then
-    vim.keymap.set("n", keymapping, builtin.colorscheme, { desc = "[C]olor[S]cheme" })
-    vim.keymap.set("n", default_colorscheme_keymapping, colorscheme.set_default_colorscheme, { desc = "Set Default Colorscheme" })
-  end
+	if opts.default_colorscheme then
+		current_colorscheme = opts.default_colorscheme
+	end
+	if opts.nvim_config then
+		colorscheme_file = opts.nvim_config .. "/colorscheme"
+	end
+	if opts.keymapping then
+		keymapping = opts.keymapping
+	end
+	local builtin_loaded, builtin = pcall(require, "telescope.builtin")
+	if builtin_loaded then
+		vim.keymap.set("n", keymapping, builtin.colorscheme, { desc = "[C]olor[S]cheme" })
+		vim.keymap.set(
+			"n",
+			default_colorscheme_keymapping,
+			colorscheme.set_default_colorscheme,
+			{ desc = "Set Default Colorscheme" }
+		)
+	end
 end
 
 colorscheme.save = function(selected_colorscheme)
-  local file, err = io.open(colorscheme_file, "w")
-  if file then
-    file:write(tostring(selected_colorscheme))
-    file:close()
-    print(selected_colorscheme .. " saved as the default colorscheme")
-  else
-    print("Could not save colorschema: ", err)
-  end
+	local file, err = io.open(colorscheme_file, "w")
+	if file then
+		file:write(tostring(selected_colorscheme))
+		file:close()
+		print(selected_colorscheme .. " saved as the default colorscheme")
+	else
+		print("Could not save colorschema: ", err)
+	end
 end
 
 colorscheme.toggle_background = function()
-  if vim.o.background == "dark" then
-    vim.o.background = "light"
-  else
-    vim.o.background = "dark"
-  end
-  if vim.g.neovide then
-    local normal_highlight = vim.api.nvim_get_hl_by_name("Normal", true)
-    if normal_highlight and normal_highlight.background then
-      vim.g.neovide_background_color = string.format("%06x", normal_highlight.background)
-      .. string.format("%x", (255 * vim.g.transparency))
-    end
-  end
+	if vim.o.background == "dark" then
+		vim.o.background = "light"
+	else
+		vim.o.background = "dark"
+	end
+	if vim.g.neovide then
+		local normal_highlight = vim.api.nvim_get_hl_by_name("Normal", true)
+		if normal_highlight and normal_highlight.background and vim.g.neovide_background_color then
+			vim.g.neovide_background_color = string.format("%06x", normal_highlight.background)
+				.. string.format("%x", (255 * vim.g.transparency))
+		end
+	end
 end
 
 local telescope_loaded, telescope = pcall(require, "telescope.config")
@@ -156,9 +164,9 @@ if telescope_loaded then
 							colorscheme.save(entry[1])
 						end
 					end,
-          ["D"] = function ()
-            colorscheme.set_default_colorscheme()
-          end
+					["D"] = function()
+						colorscheme.set_default_colorscheme()
+					end,
 				},
 				i = {
 					["<C-a>"] = function()
